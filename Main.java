40629cd4759246ec6601aefdfceb0e8647703b5e
udhayakumar v n <104062640+umeshudhaya@users.noreply.github.com>
2023-03-28 15:53:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection list=
+	/*
+        ArrayList<Integer> b=new ArrayList<Integer>();
+        b.add(1);
+        b.add(71);
+        b.add(27);
+        b.add(57);
+        b.add(7);
+        
+        Iterator v = b.iterator();
+        while (v.hasNext()){
+            System.out.println(v.next());
+
+
+        ArrayList<String> list1 = new ArrayList<String>();
+         list1.add("harish");
+         list1.add("mohan");
+         list1.add("rajesh");
+         list1.add("anish");
+         list1.add("kumar");
+        Collections.sort(list1);
+        for (String name:list1) {
+            System.out.println(name);
+        }
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.add(8);
+        list2.add(9);
+        list2.add(34);
+        list2.add(2);
+        list2.add(99);
+        Collections.sort(list2);
+        for (Integer num:list2) {
+            System.out.println(num);
+
+	 */
+       char ch;
+        for (int i = 65; i <=90; i++) {
+            ch=(char)i;
+            System.out.print(ch+" ");
+        }
+
+        }
+
+    }
+
